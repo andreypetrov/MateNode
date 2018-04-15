58a196fdcdadde26e75a6a0f8bd3ab4e17577f98
ANDREY\Andrey <petrov.development@gmail.com>
2018-04-14 21:10:02 -0400

    extracted question and answer models in separate files to reduce code repetition

--- model/exam.js
+++ model/exam.js
@@ -1,32 +1,18 @@
 const mongoose = require('mongoose');
+const question = require('../model/question');
 
+// TODO: if needed define map schema and model in a separate file
 // var mapSchema = new mongoose.Schema({
 //
 // });
 
-const answerSchema = new mongoose.Schema({
-    _id: String,
-    text: String
-});
-
-const questionSchema = new mongoose.Schema({
-    _id: String,
-    text: String,
-    correctAnswer: String,
-    answers:[answerSchema]
-});
-
 const examSchema = new mongoose.Schema({
-    //_id: mongoose.Schema.Types.ObjectId,
     subjectCode: String,
     subjectName:String,
     variant: String,
     map: {},
-    questions:[questionSchema]
+    questions:[question.schema]
 });
-
-
-
 
 mongoose.model('Exam', examSchema);
 module.exports = mongoose.model('Exam');
---
+++ model/question.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const answer = require('../model/answer');
+
+const questionSchema = new mongoose.Schema({
+    _id: String,
+    text: String,
+    imageUrl: String,
+    correctAnswer: String,
+    answers:[answer.schema],
+    givenAnswer:String
+});
+
+mongoose.model('Question', questionSchema);
+module.exports = mongoose.model('Question');
